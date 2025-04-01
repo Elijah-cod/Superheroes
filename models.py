f2c52499efbc7986c94033c9b3b57f5d58c39c17
Elijah <iamngunjiri@gmail.com>
2025-04-01 17:46:25 +0300

    Created the classes

--- models.py
+++ models.py
@@ -1,1 +1,20 @@
 from app import db
+
+class Hero(db.Model):
+    __tablename__ = 'heroes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+class Power(db.Model):
+    __tablename__ = 'powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+
+class HeroPower(db.Model):
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String, nullable=False)
