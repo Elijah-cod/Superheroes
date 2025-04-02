cd51303f1ebbf2c8ef7849843cff7fb1de7c4f2e
Elijah <iamngunjiri@gmail.com>
2025-04-02 11:51:32 +0300

    Updated the database with data from the seed file

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ class Hero(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
+    super_name = db.Column(db.String, nullable=False)
     hero_powers = db.relationship('HeroPower', back_populates='hero', cascade="all, delete")
 
     def to_dict(self, depth=1):
